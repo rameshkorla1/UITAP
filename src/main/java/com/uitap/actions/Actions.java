1fc760cbcedae4a3332afabdfb63374f2cbe09e0
rameshkorla <rameshkorla.udemy@gmail.com>
2022-10-30 12:31:40 +0530

    update the code

--- src/main/java/com/uitap/actions/Actions.java
+++ src/main/java/com/uitap/actions/Actions.java
@@ -2,7 +2,6 @@ package com.uitap.actions;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
@@ -14,10 +13,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Actions 
